407801835fbd109af4f851a998681bdc026741a3
JoaquinManchenoP <manchenojj@g.cofc.edu>
2023-01-05 13:38:42 -0500

    more layout fixes

--- src/components/JobCard/JobCard.svelte
+++ src/components/JobCard/JobCard.svelte
@@ -1,3 +1,7 @@
+<script>
+	import SkillTag from '../skillTag/SkillTag.svelte';
+</script>
+
 <div class="job__card">
 	<div class="container h-40 w-full bg-blue-500">
 		<div class="title__container h-1/5 w-full flex bg-purple-400">
@@ -5,7 +9,24 @@
 			<div class="employer h-full w-3/12 bg-orange-500">google</div>
 		</div>
 		<div class="description h-4/5 w-full flex">
-			<div class="right__side h-full w-9/12 bg-purple-500">right side</div>
+			<div class="right__side h-full w-9/12 bg-purple-500">
+				<div class="top h-2/6 w-full bg-orange-400 flex">
+					<div class="job__type h-full w-2/6 flex items-center justify-center bg-green-400">
+						Development
+					</div>
+					<div
+						class="skills flex items-center justify-center w-4/6 space-x-2 min-w-fit bg-yellow-400"
+					>
+						<SkillTag />
+						<SkillTag />
+						<SkillTag />
+						<SkillTag />
+						<SkillTag />
+						<SkillTag />
+						<SkillTag />
+					</div>
+				</div>
+			</div>
 			<div class="right__side h-full w-3/12 bg-red-500">left side</div>
 		</div>
 	</div>
---
+++ src/components/skillTag/SkillTag.svelte
@@ -0,0 +1,1 @@
+<div class="skill h-7 w-10 bg-gray-500">skill</div>
